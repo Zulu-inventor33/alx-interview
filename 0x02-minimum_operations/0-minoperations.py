b4325a5f7c3d14f153ccc6ab1ae1a892c98f6c82
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-10-17 16:01:51 +0000

    minimum number operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+"""Min operations"""
+
+
 def minOperations(n):
+    """
+    Function that returns the minimum number of operations
+
+    Args:
+        n (int): number of elements
+
+    Returns:
+        int: minimum number of operations
+    """
     if n <= 1:
         return 0
 
-    operations = 0
-    divisor = 2
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
 
-    # Factor n by its prime factors
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor  # Increment operations by the current divisor
-            n //= divisor          # Divide n by the divisor
-        divisor += 1              # Move to the next potential prime factor
-
-    return operations
-
+    return n
