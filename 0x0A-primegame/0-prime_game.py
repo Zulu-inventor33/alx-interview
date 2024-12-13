7fd623e2e9e2c3f89917a4e7b79d9c328c9e71f5
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-12-13 01:49:15 +0000

    making the changes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
+"""The Prime game module.
+"""
+
 
 def isWinner(x, nums):
-    def is_prime(n):
-        """Helper function to check if a number is prime."""
-        if n < 2:
-            return False
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    def get_prime_count(n):
-        """Counts the number of prime numbers up to n."""
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-        return sum(primes)
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        prime_count = get_prime_count(n)
-        if prime_count % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    return None
-
-
-if __name__ == "__main__":
-    pass
+    """Determining the winner of a prime game session with `x` rounds.
+    """
+    if x < 1 or not nums:
+        return None
+    marias_wins, bens_wins = 0, 0
+    # generate primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
